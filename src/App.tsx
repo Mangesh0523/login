b5ebb59c5a0d009310f59a280fd660f69ad8de23
mangesSh <bhaganagaremangesh@gmail.com>
2022-11-03 11:38:18 +0530

    loginform

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
+
 import './App.css';
 
 function App() {
+  
+  
+
+var visible = 0;
+const el = document.getElementById('inputs');
+const btn = document.getElementById('eye');
+
+const  handleClick2=()=>{
+
+  if (el != null && btn != null) {
+    btn.addEventListener('click', function handleClick() {
+      if (el.style.display === 'none') {
+        // ✅ Shows element if hidden
+        el.style.display = 'block';
+  
+        btn.textContent = 'Hide element';
+      } else {
+        // ✅ Hides element if shown
+        el.style.display = 'none';
+  
+        btn.textContent = 'Show element';
+      }
+     
+      
+    });
+  }
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div className="Login">
+     <div className='Loginbox'>
+      <div className='Loginheader'>Login </div>
+        <div className='inputs'>
+        <input className='email' placeholder='Enter your email'/>
+        {/* <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"/> */}
+      
+          <input
+        
+        className='password' placeholder='Enter your password'
+        type={"password"} onClick={handleClick2}
+        
+        />
+        
+
+        {/* <i className="fa fa-eye" id="eye"  onClick={handleClick2}  ></i>  */}
+      
+        
+       
+         
+      </div>
+      <div style={{textAlign:"center",color:"red"}}>
+
+        <button  className='submitbutton' >Login</button>
+        <p>Need an account? <br/>
+        <span className='line'>
+          {/*put rauter link here*/}
+          <a href='#'>Sign up</a>
+        </span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       
+      </div>
+     </div>
     </div>
   );
 }
 
 export default App;
+function showPass(arg0: () => void) {
+  throw new Error('Function not implemented.');
+}
+
